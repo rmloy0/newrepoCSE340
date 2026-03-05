26632ba737c4dcea85ab64d295c19b3a673b058e
rmloyo <rmloyo@gmail.com>
2026-03-04 20:35:42 -0600

    first commit

--- server.js
+++ server.js
@@ -9,11 +9,26 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
+
+
+/* ***********************
+ * vie Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(require("./routes/static"))
+
+//Index route
+
+app.get("/", function (req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
@@ -28,3 +43,4 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
