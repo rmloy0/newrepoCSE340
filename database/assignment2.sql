4c47b847fbeb8cc717e5be80b00703b425cb9627
rmloyo <rmloyo@gmail.com>
2026-04-11 13:41:36 -0600

    database re built

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -37,3 +37,23 @@ WHERE inventory.classification_id = 2;
 UPDATE public.inventory
 SET inv_image = REPLACE(inv_image,  '/images/','/images/vehicles/'),  
  inv_thumbnail = REPLACE(inv_thumbnail,  '/images/','/images/vehicles/');
+
+
+ 
+CREATE TABLE favorites (
+  account_id INTEGER NOT NULL,
+  inv_id INTEGER NOT NULL,
+  note TEXT,
+
+  CONSTRAINT favorites_pk PRIMARY KEY (account_id, inv_id),
+
+  CONSTRAINT favorites_account_fk
+    FOREIGN KEY (account_id)
+    REFERENCES account(account_id)
+    ON DELETE CASCADE,
+
+  CONSTRAINT favorites_inventory_fk
+    FOREIGN KEY (inv_id)
+    REFERENCES inventory(inv_id)
+    ON DELETE CASCADE
+);
